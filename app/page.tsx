3e3a8a6e817dfdc7c7cab60385b5030f9ab4f97c
Farhan2323 <89047823+Farhan2323@users.noreply.github.com>
2024-08-17 15:11:57 -0500

    commit 4
    Styled main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
 'use client'
-import { TextField } from "@/node_modules/@mui/material/index";
+import { ThemeProvider, createTheme,  } from '@mui/material/styles';
+import { Button, TextField, Typography, Box, Grid } from "@/node_modules/@mui/material/index";
+import { AxiosResponse } from "@/node_modules/axios/index";
 import React, {useState, ChangeEvent} from "react";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1290e2', // Change this to your desired primary color
+    },
+    secondary: {
+      main: '#e26512', // Change this to your desired secondary color
+    },
+    background: {
+      default: '#12e2cd', // Change the default background color
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '2.5rem',
+      fontWeight: 600,
+      color: '#333', // Custom color for h1
+    },
+    h2: {
+      fontSize: '2rem',
+      fontWeight: 500,
+      color: '#333', // Custom color for h2
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
+const axios = require("axios")
 
 export default function Home() {
   const [userInput, setuserInput] = useState<string>('');
@@ -8,14 +46,69 @@ export default function Home() {
     setuserInput(event.target.value);
   }
 
+  const [output , setOutput] = useState<schema>()
 
-  return (
-    <div>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" 
-        value = {userInput} 
-        onChange={inputHandler} />
-        {userInput}
-    </div>
+  interface schema {
+    title: string,
+    summary: string 
+  }
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
+  const scrape = async() =>{
+    try{
+      const response: AxiosResponse<schema> = await axios.post(`${apiUrl}/scrape` , {url : userInput})
+      console.log(response.data)
+      setOutput(response.data)
+
+    }catch (error){
+      console.error("NO fetching")
+
+    }
     
+    
+    
+    
+
+  }
+
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' , backgroundColor: '#8dc6f0' }}>
+        <Grid container direction="column" alignItems="center" spacing={2}>
+          <Grid item>
+            <Typography variant="h1" component="h1" gutterBottom>
+              Olostep-Track Web Scraper
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Grid container alignItems="center" spacing={2}>
+              <Grid item>
+                <TextField
+                  id="outlined-basic"
+                  label="Enter URL here!"
+                  variant="outlined"
+                  value={userInput}
+                  onChange={inputHandler}
+                  color="primary"
+                  
+                />
+              </Grid>
+              <Grid item>
+                <Button color='primary' variant="contained" onClick={scrape}>
+                  Scrape!
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="h1"> Title: {output?.title}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h3"> Site Summary: {output?.summary}</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
 }
