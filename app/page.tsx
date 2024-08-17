92ccf77201650e47cc391c7333a6c984807b8f0e
Farhan2323 <89047823+Farhan2323@users.noreply.github.com>
2024-08-17 13:08:38 -0500

    Commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-
+'use client'
+import { TextField } from "@/node_modules/@mui/material/index";
+import React, {useState, ChangeEvent} from "react";
 
 export default function Home() {
+  const [userInput, setuserInput] = useState<string>('');
+  const inputHandler = (event : ChangeEvent<HTMLInputElement>) => {
+    setuserInput(event.target.value);
+  }
+
+
   return (
-    <div></div>
+    <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" 
+        value = {userInput} 
+        onChange={inputHandler} />
+        {userInput}
+    </div>
     
   );
 }
